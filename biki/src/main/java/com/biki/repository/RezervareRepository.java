c613db797e8c4d9387353a2b5b9264b7989c9393
Bican-Valeriu-Cristian <126001331+Bican-Valeriu-Cristian@users.noreply.github.com>
2024-12-06 15:34:15 +0200

    Delete biki directory

--- biki/src/main/java/com/biki/repository/RezervareRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biki.repository;
-
-import com.biki.model.Rezervare;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RezervareRepository extends JpaRepository<Rezervare, Long> {
-}
